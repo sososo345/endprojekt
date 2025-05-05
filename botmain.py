834408574655d8b5e9859759fc24fa9fafb0ad2e
sososo345 <zh.gadzevich@mail.ru>
2025-05-05 16:38:39 +0300

    Add files via upload

--- botmain.py
+++ botmain.py
@@ -1,3 +1,4 @@
+import json
 import asyncio
 import pandas
 from aiogram import Bot, Dispatcher, types
@@ -13,14 +14,27 @@ dp.include_router(router)
 
 data = pandas.read_json("cars.json").to_dict()
 
+def sell_car_process(data):
+    print(777)
+    with open("cars.json","w",encoding="UTF-8")as f:
+        json.dump(data,f)
+
 
 @dp.message(Command("start"))
 async def start(message:types.Message):
-    await message.answer(f'{data}')
+    await message.answer('ffgg')
 
+@dp.message(Command("sell_car"))
+async def sell_car(message:types.Message):
+    sell_car_process(data)
+
+@dp.message(Command("buy_car"))
+async def start(message:types.Message):
+    await message.answer(f'{data}')
 
 
 async def main():
     print('Запкск бота')
     await dp.start_polling(bot)
 asyncio.run(main())
+
